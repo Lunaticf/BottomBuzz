76a4b65830c5d35c93b6f5c1c1abf00920225ab9
lunaticf <724922441@qq.com>
2019-06-13 14:38:23 +0800

    init

---
+++ src/main/java/com/lunaticf/BottomBuzz/BottomBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.lunaticf.BottomBuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottomBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottomBuzzApplication.class, args);
+	}
+
+}
